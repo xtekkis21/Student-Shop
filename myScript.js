dc232a0f686d0a363cb9c4c0e5af6809f6030182
xtekkis21 <xtekkis@uclan.ac.uk>
2025-12-16 18:37:05 +0200

    Update myScript.js
    product array

--- myScript.js
+++ myScript.js
@@ -7,3 +7,16 @@ if (hamburger && mobileMenu){
 	mobileMenu.classList.toggle('show');
 });
 }
+
+//list of tshirts
+const tshirts = [
+['Legacy T-Shirt','Red','£7.99','good-stock','resources_2025/resources/images/tshirts/tshirt1.jpg','Perfect for those graduating this year. Get a bargain whilst we have the stock.'],
+['Legacy T-Shirt','Green','£7.99','last-few','resources_2025/resources/images/tshirts/tshirt2.jpg','Limited stock. Grab these nostalgic items before they make their way onto eBay.'],
+['Legacy T-Shirt','Blue','£7.99','out-of-stock','resources_2025/resources/images/tshirts/tshirt3.jpg','Sadly we are sold out of this legendary item. Keep an eye out for future stock.'],
+['Legacy T-Shirt','Cyan','£7.99','good-stock','resources_2025/resources/images/tshirts/tshirt4.jpg','Perfect for those graduating this year. Get a bargain whilst we have the stock.'],
+['Legacy T-Shirt','Magenta','£7.99','out-of-stock','resources_2025/resources/images/tshirts/tshirt5.jpg','Sadly we are sold out of this legendary item. Keep an eye out for future stock.'],
+['Legacy T-Shirt','Yellow','£7.99','last-few','resources_2025/resources/images/tshirts/tshirt6.jpg','Limited stock. Grab these nostalgic items before they make their way onto eBay.'],
+['Legacy T-Shirt','Black','£7.99','out-of-stock','resources_2025/resources/images/tshirts/tshirt7.jpg','Sadly we are sold out of this legendary item. Keep an eye out for future stock.'],
+['Legacy T-Shirt','Grey','£7.99','good-stock','resources_2025/resources/images/tshirts/tshirt8.jpg','Perfect for those graduating this year. Get a bargain whilst we have the stock.'],
+['Legacy T-Shirt','Burgundy','£7.99','last-few','resources_2025/resources/images/tshirts/tshirt9.jpg','Limited stock. Grab these nostalgic items before they make their way onto eBay.'],
+];
