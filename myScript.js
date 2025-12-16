085db0f6dcde3e1b05dc4fa9cf8c469c0bd327fe
xtekkis21 <xtekkis@uclan.ac.uk>
2025-12-16 18:36:47 +0200

    Update myScript.js
    hamburger menu

--- myScript.js
+++ myScript.js
@@ -1,1 +1,9 @@
-
+//HAMBURGER MENU
+const hamburger = document.getElementById('hamburger-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+//toggle visibility when clicked
+if (hamburger && mobileMenu){
+	hamburger.addEventListener('click', () => {
+	mobileMenu.classList.toggle('show');
+});
+}
