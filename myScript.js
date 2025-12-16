417c9debcd4b47912a2d9c935fe1d56072ed388f
xtekkis21 <xtekkis@uclan.ac.uk>
2025-12-16 18:37:28 +0200

    Update myScript.js
    product page

--- myScript.js
+++ myScript.js
@@ -20,3 +20,54 @@ const tshirts = [
 ['Legacy T-Shirt','Grey','£7.99','good-stock','resources_2025/resources/images/tshirts/tshirt8.jpg','Perfect for those graduating this year. Get a bargain whilst we have the stock.'],
 ['Legacy T-Shirt','Burgundy','£7.99','last-few','resources_2025/resources/images/tshirts/tshirt9.jpg','Limited stock. Grab these nostalgic items before they make their way onto eBay.'],
 ];
+
+//PRODUCT PAGE
+
+const products= document.getElementById('products'); //Get the product with id "products"
+const stockFilter= document.getElementById('stock-filter'); //Get the stock filter with id "stock"
+if(products){ //only running if we are on the products page
+	function showProducts(filter){ //render all products filtered by stock status
+		products.innerHTML=''; //clear existing cards
+		//loop through each tshirt in the array
+		tshirts.forEach(([name, color, price, stock, imgSrc, desc], index)=>{
+			const stockLower = stock.toLowerCase();
+			let show = true;
+			//filter through in stock
+			if(filter === 'in'){
+				show = !stockLower.includes('out');
+			//filter through out of stock
+			} else if (filter === 'out'){
+				show = stockLower.includes('out')
+			}
+			if(!show)return; 
+			
+			//create the product card
+			const card=document.createElement('div');
+			card.className='product-itself';
+			
+			//build the html inside of js
+			card.innerHTML=`
+			<img src="${imgSrc}" alt="${name} - ${color}">
+			<h3>${name} - ${color}</h3>
+			<p>${desc}
+			<a href="item.html" class= "read-more"
+			onclick="sessionStorage.setItem('selectedProduct', ${index})">View more</a>
+			</p>
+			<p><strong>${price}</strong></p>
+			<p class="stock-status ${stock}">${stock.replace(/-/g, ' ')}</p>
+			<button class="buy-button" onclick="addToCart(${index})">Add to Cart</button>
+			`;
+			
+			//Add this card into the products
+			products.appendChild(card)
+			});
+	}
+	//load the products
+	showProducts('all');
+	//refilter products when selection changes
+	if(stockFilter){
+		stockFilter.addEventListener('change', (e) => {
+		showProducts(e.target.value);
+		});
+	}
+}
